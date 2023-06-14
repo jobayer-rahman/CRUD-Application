76d613ae898c20644f25270adb25140e133ae403
jobayer-rahman <jobayer_rahman@yahoo.com>
2023-06-14 15:11:12 +0600

    Added the serializers for the model

---
+++ blogAPI/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Author, Comment, Post
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'first_name', 'last_name', 'phone_number', 'email']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'slug', 'author', 'body', 'created_at']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'post', 'author', 'created_at']
