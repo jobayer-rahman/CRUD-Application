0bf9f1f883956f870dd15088a6e1e584c222f9ff
jobayer-rahman <jobayer_rahman@yahoo.com>
2023-06-14 14:56:02 +0600

    Added the database models

--- blogAPI/models.py
+++ blogAPI/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=14)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,
+                               related_name='posts')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
