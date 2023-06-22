ea707c352258df19b2b49a9a3305ab231582f3e1
jobayer-rahman <jobayer_rahman@yahoo.com>
2023-06-22 13:13:19 +0600

    Added mysql database

--- siteManager/settings.py
+++ siteManager/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'siteManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_app_db',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'PASSWORD': 'enter_your_password_here'
     }
 }
 
